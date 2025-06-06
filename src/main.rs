8a5c8aa8d885f2da411f4b5535b5efc90075e084
ChenRuiwei <1982833213@qq.com>
2025-06-07 02:37:17 +0800

    feat(tracing): support logging with tracing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod app;
 mod input;
 mod keys;
+mod tracing;
 mod utils;
 
 use app::App;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+    tracing::initialize_logging()?;
     let terminal = ratatui::init();
     let result = App::new().run(terminal);
     ratatui::restore();
